8367c22482ed57a9d3144144f3b1f18a55a90feb
seongjaemoon <seongjae.moon.92@gmail.com>
2019-02-16 18:53:49 +0000

    movement of the block

--- scripts/main.js
+++ scripts/main.js
@@ -19,11 +19,17 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   const pageWrapper = document.querySelector('.page-wrapper')
-  const gridItems = document.querySelectorAll('.grid')
+  // const gridItems = document.querySelectorAll('.grid')
+  const gridItems = pageWrapper.childNodes
+
+  function gameSetup(){
+    divCreation(200)
+    arrayCreation()
+  }
 
   function arrayCreation(){
     const x = []
-    for(let i = 0; i < 10; i++) {
+    for(let i = 0; i < 20; i++) {
       x.push([])
       for(let j = 0; j < 10; j++) {
         x[i].push(0)
@@ -35,25 +41,54 @@ window.addEventListener('DOMContentLoaded', () => {
   function divCreation(num) {
     for (let i = 0; i < num; i++){
       const newDivs = document.createElement('div')
+      const newText = document.createTextNode(i)
+      newDivs.dataset.array = i
       newDivs.setAttribute('class', 'grid')
+      newDivs.appendChild(newText)
       pageWrapper.appendChild(newDivs)
     }
   }
 
+  gameSetup()
+
   // class Shape{
-  //   constructor(){
-  //     this.piece = gridItems[16]
-  //     this.color = gridItems[16].style.backgroundColor = 'red'
+  //   constructor(i, color){
+  //     this.index = document.querySelector('[data-array="'+ i + '"]')
+  //     this.color = this.index.style.backgroundColor = color
+  //     // this.color = this.shape.forEach(col => col.style.backgroundColor = color
   //   }
   // }
-  // gridItems[16].style.backgroundColor = 'red'
 
-  function init(){
-    divCreation(200)
-    arrayCreation()
-  }
 
-  console.log(gridItems.item(16).style.backgroundColor = 'red')
+  let i = 14
+  let pick
 
-  init()
+  document.addEventListener('keydown', (e)=>{
+    if (e.keyCode === 37){
+      i--
+      console.log(e.keyCode)
+    }
+    if (e.keyCode === 39){
+      i++
+      console.log(e.keyCode)
+    }
+    if (e.keyCode === 40){
+      i +=10
+      console.log(e.keyCode)
+    }
+    console.log(i)
+    pick = gridItems.item(i)
+    pick.style.backgroundColor = 'red'
+  })
+
+
+
+
+
+
+  // let pick = document.querySelector('[data-array="' + i + '"]')
+
+  // const centerPiece = new Shape(15, 'red')
+
+  console.log([gridItems[4], gridItems[14],gridItems[15],gridItems[16]])
 })
