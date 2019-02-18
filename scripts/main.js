1711b89ecfe0b38cbd57520fef751beef96a6d06
seongjaemoon <seongjae.moon.92@gmail.com>
2019-02-18 15:21:42 +0000

    bottom border is working

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,11 @@
 window.addEventListener('DOMContentLoaded', () => {
   const pageWrapper = document.querySelector('.page-wrapper')
   const gridItems = pageWrapper.childNodes
-  let arrowKey = null
-
   console.log(gridItems)
   function gameSetup(){
     boardDivs(200)
     boardArray()
   }
-
   function boardArray(){
     const x = []
     for(let i = 0; i < 200; i++) {
@@ -16,7 +13,6 @@ window.addEventListener('DOMContentLoaded', () => {
     }
     console.log(x)
   }
-
   function boardDivs(num) {
     for (let i = 0; i < num; i++){
       const newDivs = document.createElement('div')
@@ -26,9 +22,7 @@ window.addEventListener('DOMContentLoaded', () => {
       newDivs.appendChild(newText)
       pageWrapper.appendChild(newDivs)
     }
-    // console.log(pageWrapper)
   }
-
   gameSetup()
 
   class Shape {
@@ -36,48 +30,50 @@ window.addEventListener('DOMContentLoaded', () => {
       this.newIndex = null
       this.emptyArray = []
       this.newShape = null
-      // this.falling()
     }
-
     falling(){
       setInterval(()=>{
-        clear()
-        this.emptyArray = []
-        for (let i = 0; i < this.index.length; i++){
-          if(this.index[i] < 200){
+        if (this.index[0] < 180 || this.index[1] < 180 || this.index[2] < 190 || this.index[3] < 190) {
+          clear()
+          this.emptyArray = []
+          for (let i = 0; i < this.index.length; i++){
             this.index[i] += 10
             this.newIndex = this.index[i]
             this.newShape = gridItems[this.newIndex]
             this.emptyArray.push(this.newShape)
           }
+          this.fill()
+          console.log(this.newIndex)
+          return this.newIndex
         }
-        this.fill()
       },1000)
     }
-
     fill(){
       this.emptyArray.forEach(shapeIndex => shapeIndex.classList.add('filled'))
     }
-
     movement(e){
-      this.emptyArray = []
-      for (let k = 0; k < this.index.length; k++){
-        if (e.keyCode === 37){
-          this.index[k]--
-        } else if (e.keyCode === 39) {
-          this.index[k]++
-        } else if(e.keyCode === 40) {
-          this.index[k]+= 10
-        } else {
-          return false
+      if (this.index[0] < 180 || this.index[1] < 180 || this.index[2] < 190 || this.index[3] < 190) {
+        this.emptyArray = []
+        for (let k = 0; k < this.index.length; k++){
+          if (e.keyCode === 37){
+            this.index[k]--
+          } else if (e.keyCode === 39) {
+            this.index[k]++
+          } else if(e.keyCode === 40) {
+            this.index[k]+= 10
+          } else {
+            return false
+          }
+          this.newIndex = this.index[k]
+          this.newShape = gridItems[this.newIndex]
+          this.emptyArray.push(this.newShape)
         }
-        this.newIndex = this.index[k]
-        this.newShape = gridItems[this.newIndex]
-        this.emptyArray.push(this.newShape)
+        clear()
+        this.fill()
       }
-      clear()
-      this.fill()
     }
+
+
   }
 
   class ShapeL extends Shape {
@@ -87,7 +83,6 @@ window.addEventListener('DOMContentLoaded', () => {
       this.index = [4, 14, 15, 16]
     }
   }
-
   class ShapeLAlternate extends Shape {
     constructor(){
       super()
@@ -95,7 +90,6 @@ window.addEventListener('DOMContentLoaded', () => {
       this.index = [6, 14, 15, 16]
     }
   }
-
   class ShapeZ extends Shape {
     constructor(){
       super()
@@ -103,7 +97,6 @@ window.addEventListener('DOMContentLoaded', () => {
       this.index = [4, 5, 15, 16]
     }
   }
-
   class ShapeZAlternate extends Shape {
     constructor(){
       super()
@@ -111,7 +104,6 @@ window.addEventListener('DOMContentLoaded', () => {
       this.index = [5, 6, 14, 15]
     }
   }
-
   class ShapeO extends Shape {
     constructor(){
       super()
@@ -119,7 +111,6 @@ window.addEventListener('DOMContentLoaded', () => {
       this.index = [4, 5, 14, 15]
     }
   }
-
   class ShapeT extends Shape {
     constructor(){
       super()
@@ -127,7 +118,6 @@ window.addEventListener('DOMContentLoaded', () => {
       this.index = [5, 14, 15, 16]
     }
   }
-
   class ShapeI extends Shape {
     constructor(){
       super()
@@ -135,7 +125,6 @@ window.addEventListener('DOMContentLoaded', () => {
       this.index = [13, 14, 15, 16]
     }
   }
-
   const shape1 = new ShapeL()
   const shape2 = new ShapeLAlternate()
   const shape3 = new ShapeZ()
@@ -150,7 +139,6 @@ window.addEventListener('DOMContentLoaded', () => {
     const randomIndex = Math.floor(Math.random()*randomArray.length)
     const newBlock = randomArray[randomIndex]
     arrayBlocks.push(newBlock)
-    // console.log(arrayBlocks)
     return arrayBlocks
   }
 
@@ -165,7 +153,5 @@ window.addEventListener('DOMContentLoaded', () => {
   document.addEventListener('keydown', (e) => {
     arrayBlocks[arrayBlocks.length-1].movement(e)
   })
-
-  // let pick = document.querySelector('[data-array="' + i + '"]')
 
 })
